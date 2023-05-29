42c863d167b853d7171155f46d14a6d9f54ffd78
Vasilii Salikov <vaseck174@gmail.com>
2023-05-29 15:53:22 +0300

    fix: fix data preparation script

--- medretqna/data_preparation.py
+++ medretqna/data_preparation.py
@@ -18,25 +18,26 @@ if __name__ == "__main__":
 
     # data loading and preparation
     data = load_data(
-        "medmcqa", sample_size=prep_config["sample_size"], seed=prep_config["seed"]
+        "medmcqa", sample_size=prep_config["sample_size_per_db"], seed=prep_config["seed"]
     )
     data1 = load_data(
         "AnonymousSub/MedQuAD_47441_Question_Answer_Pairs",
-        sample_size=prep_config["sample_size"],
+        sample_size=prep_config["sample_size_per_db"],
         seed=prep_config["seed"],
     )
     data = data.append(data1)
+    data = list(set(data["A"]))
+
+    log.info("Final combined data length: {}".format(len(data)))
 
     # embeddings model loading and data encoding
     device = "cuda:0" if torch.cuda.is_available() else "cpu"
     emb_model = EmbedModel(
-        back_config["emb"], device=device if prep_config["device"] else "cpu"
+        back_config["emb"], device=device if prep_config["device"] == "system" else "cpu"
     )
-    ansdb = AnswerDB(list(set(data["A"]))).encode(
-        emb_model, tqdm_flag=True, batch_len=prep_config["batch_len"]
+    ansdb = AnswerDB(data=data).encode(
+        emb_model, tqdm_flag=True, batch_len=prep_config["batch_len"], final_device="cpu"
     )
-
-    log.info("Ans embedded shape: {}".format(ansdb.embedded.shape))
 
     # saving data
     with open(back_config["db"], "wb") as file:
